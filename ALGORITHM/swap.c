2ad3078c06a242df400702531209bd2849d82685
urbashi123 <urvashi.agrawalla123@gmail.com>
2018-10-07 01:05:04 +0530

    some minor changes

--- ALGORITHM/swap.c
+++ ALGORITHM/swap.c
@@ -2,18 +2,13 @@
 #include<stdio.h>
 int main()
 {
-    int a,b,c;
+    int x=5,y=10;
+    x=x+y;
+    y=x-y;
+    x=x-y;
+    printf("after swapping: x= %d,y = %d",x,y);
 
-    scanf("%d" , &a);
-    scanf("%d" , &b);
-    printf("First Number:%d",a);
-    printf("Second Number:%d",b);
-    
-    c=a;
-    a=b;
-    b=c;
-    printf("First Number:%d",a);
-    printf("Second Number:%d",b);
+    return 0;
 
 
 }
